08b2e2364310a8ea42bb37bb0da0702bd383f3e5
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-06-19 18:53:37 -0400

    add another test

--- __tests__/getMaxWaterContainer.js
+++ __tests__/getMaxWaterContainer.js
@@ -4,6 +4,10 @@
  * @returns 
  */
 const getMaxWaterContainer = (numbers) => {
+    if (numbers.length === 0 || numbers.length === 1) {
+        return 0;
+    }
+
     let existingMaxArea = 0;
     for (let i = 0; i < numbers.length; i++) {
         for (let j = i + 1; j < numbers.length; j++) {
@@ -27,5 +31,9 @@ describe('cup_brute_force', () => {
     it('should return 28, with following example', () => {
         const actual = getMaxWaterContainer([7, 1, 2, 3, 9]);
         expect(actual).toEqual(28);
+    });    
+    it('should return 28, with following example', () => {
+        const actual = getMaxWaterContainer([1, 8, 6, 2, 5, 4, 8, 3, 7]);
+        expect(actual).toEqual(49);
     });
 });
