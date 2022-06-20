d4aa00071a4946349f97a61cc9a68046f8573846
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-06-20 12:30:37 -0400

    setup the test scaffolding and expectation

--- __tests__/trappingRainwater.js
+++ __tests__/trappingRainwater.js
@@ -2,17 +2,32 @@
 /**
  * 
  * @param {Number[]} nums 
+ * @return Number
  */
 const trappingRainwaterBruteForce = (nums) => {
+    let totalWater = 0;
+    let pointer = 0;
+    let maxL = 0;
+    let maxR = 0;
 
+    while (pointer < nums.length) {
+
+
+
+        pointer++;
+    }
+
+
+    return totalWater;
 };
 
 
 describe('trappingRainwater', () => {
     describe('trappingRainwaterBruteForce', () => {
-        it('', () => {
+        it('should return 8 with the given array', () => {
+            const actual = trappingRainwaterBruteForce([0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]);
 
+            expect(actual).toEqual(8);
         });
-    })
-
+    });
 });
