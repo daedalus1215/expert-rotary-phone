ee1bbcddb4b4fa98923d1a378fb1c942af7f8252
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-07-01 18:57:46 -0400

    spike in tortoise and hare

---
+++ __tests__/linkedLists/tortoiseAndHare.js
@@ -0,0 +1,24 @@
+const tortoiseAndHare = head => {
+    let tortoise = head;
+    let hare = head;
+    hare.next;
+    while (true) {
+        tortoise = tortoise.next;
+        hare = hare.next
+        if (hare === tortoise) break;
+        if (hare === null) return false;
+        if (hare.next !== null) {
+            hare = hare.next;
+            if (hare === tortoise) break;
+        }
+    }
+    p1 = head;
+    while (hare !== p1) {
+        hare = hare.next;
+        p = p.next;
+        hare.next;
+    }
+
+    return p1;
+
+}
