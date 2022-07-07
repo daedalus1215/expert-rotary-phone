9a8d998727a3cdc5dfc18be1f98c7dcfc31d9b0a
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-07-06 21:28:34 -0400

    remove console logs from mergeSort / rewrite it

--- __tests__/sort/mergeSort.js
+++ __tests__/sort/mergeSort.js
@@ -1,55 +1,43 @@
-const mergeSort = array => {
+const mergeSort = (array) => {
     if (array.length === 1) {
         return array;
     }
 
-    const length = array.length;
-    const middle = Math.floor(length / 2);
+    const middle = array.length / 2;
     const left = array.slice(0, middle);
     const right = array.slice(middle);
-
-    console.log('left', left);
-    console.log('right', right);
 
     return merge(
         mergeSort(left),
         mergeSort(right)
-    );
-}
-
-// [1,2,3,4]
+    )
+};
 
 const merge = (left, right) => {
-    let result = [];
+    const result = [];
     let leftIndex = 0;
     let rightIndex = 0;
+
     while (leftIndex < left.length && rightIndex < right.length) {
         if (left[leftIndex] < right[rightIndex]) {
             result.push(left[leftIndex]);
-            console.log('push is invoked!', result)
             leftIndex++;
         } else {
-            result.push(right[rightIndex])
+            result.push(right[rightIndex]);
             rightIndex++;
         }
     }
 
-    console.log('result', result);
-
-    const yes = result
+    return result
         .concat(left.slice(leftIndex))
         .concat(right.slice(rightIndex));
-
-    console.log('result.concat', yes);
-    return yes;
-}
-
+};
 
 describe('mergeSort', () => {
     it('should', () => {
-        let array = [6, 2, 8, 1];
+        let array = [6, 2, 8, 1, 9, 3];
         const actual = mergeSort(array);
-        expect(actual).toEqual([1, 2, 6, 8]);
+        expect(actual).toEqual([1, 2, 3, 6, 8, 9]);
 
     });
 })
