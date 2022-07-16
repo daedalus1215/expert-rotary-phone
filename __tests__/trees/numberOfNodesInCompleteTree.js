296315f91252037a50bec53623d3395d47191d16
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-07-16 11:20:04 -0400

    UT getTreeHeight and fix a bug

--- __tests__/trees/numberOfNodesInCompleteTree.js
+++ __tests__/trees/numberOfNodesInCompleteTree.js
@@ -1,15 +1,25 @@
-
-
-
-
-
+const TreeNode = require('../../src/TreeNode');
 
 
 const getTreeHeight = root => {
     let height = 0;
-    while (!root.left) {
+    console.log('root',root)
+    while (root.left) {
+        console.log('height',height)
         height++;
         root = root.left;
     }
+    console.log('end')
     return height;
 }
+
+
+describe('__tests__/trees/numberOfNodesInCompleteTree', () => {
+    describe('getTreeHeight', () => {
+        it('should return...', () => {
+            const root = new TreeNode();
+            root.insert([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, null, null, null]);
+            expect(getTreeHeight(root)).toEqual(3);
+        });
+    });
+});
