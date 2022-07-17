9517966c42c4037fb9e1c75d9f23c5e777c3ca18
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-07-17 08:17:35 -0400

    Add numberOfNodesInCompleteTree to numberOfNodesInCompleteTree.js

--- __tests__/trees/numberOfNodesInCompleteTree.js
+++ __tests__/trees/numberOfNodesInCompleteTree.js
@@ -3,13 +3,13 @@ const TreeNode = require('../../src/TreeNode');
 
 const getTreeHeight = root => {
     let height = 0;
-    console.log('root', root)
+    // console.log('root', root)
     while (root.left) {
-        console.log('height', height)
+        // console.log('height', height)
         height++;
         root = root.left;
     }
-    console.log('end')
+    // console.log('end')
     return height;
 }
 
@@ -30,25 +30,51 @@ const nodeExists = (idxToFind, height, node) => {
     return node !== null;
 }
 
+const numberOfNodesInCompleteTree = (root) => {
+    if (!root) return null;
+    const height = getTreeHeight(root);
+    if (height === 0) return 1;
+    let left = 0,
+        upperCount = Math.pow(2, height) -1,
+        right = upperCount;
+
+    while (left < right) {
+        const idxToFind = Math.ceil((left + right) / 2);
+        if (nodeExists(idxToFind, height, root)) {
+            left = idxToFind;
+        } else {
+            right = idxToFind - 1;
+        }
+    }
+
+    return upperCount + left;
+}
+
 
 describe('__tests__/trees/numberOfNodesInCompleteTree', () => {
+    let root;
+    beforeEach(() => {
+        root = new TreeNode();
+        root.insert([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, null, null, null]);
+    });
+
     describe('getTreeHeight', () => {
         it('should return a height of 3 with the following Tree', () => {
-            const root = new TreeNode();
-            root.insert([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, null, null, null]);
             expect(getTreeHeight(root)).toEqual(3);
         });
     });
     describe('nodeExists', () => {
         it('should return true, if we expect there to be 4 items on the last level', () => {
-            const root = new TreeNode();
-            root.insert([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, null, null, null]);
             expect(nodeExists(4, 3, root)).toEqual(true);
         })
         it('should return false, if we expect there to be 5 items on the last level', () => {
-            const root = new TreeNode();
-            root.insert([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, null, null, null]);
             expect(nodeExists(5, 3, root)).toEqual(false);
         })
     })
+
+    describe('numberOfNodesInCompleteTree', () => {
+        it('should...', () => {
+            expect(numberOfNodesInCompleteTree(root)).toEqual(11);
+        })
+    });
 });
