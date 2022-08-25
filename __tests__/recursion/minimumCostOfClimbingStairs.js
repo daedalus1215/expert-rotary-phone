b83e2d8590bf9c2ea676590a61c67508f4b454d4
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-08-24 20:42:47 -0400

    Add minimumCostOfClimbingStairs

--- __tests__/recursion/minimumCostOfClimbingStairs.js
+++ __tests__/recursion/minimumCostOfClimbingStairs.js
@@ -1,3 +1,16 @@
-const minCostOfClimbingStairs = () => {
-    
+const minCostOfClimbingStairs = (steps) => {
+    return Math.min(costOf(steps, steps.length - 1), costOf(steps, steps.length - 2));
 };
+
+const costOf = (steps, i) => {
+    if (i <= 0) {
+        return 0
+    }
+    return steps[i] + Math.min(costOf(steps, i - 1), costOf(steps, i - 2));
+};
+
+describe('minimumCostOfClimbingStairs.js', () => {
+    it('should', () => {
+        expect(minCostOfClimbingStairs([5, 30, 15, 10,11])).toEqual(25);
+    });
+});
