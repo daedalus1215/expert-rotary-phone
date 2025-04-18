57a4e9bd84c24d3a29b6b951ede1a88401a310dc
la la <13687739+daedalus1215@users.noreply.github.com>
2025-04-17 20:47:05 -0400

    pairSum: make a functional implementation

--- __tests__/array_hash/pairSum.test.ts
+++ __tests__/array_hash/pairSum.test.ts
@@ -23,27 +23,77 @@ const pairSum = (inputs: number[], target: number) => {
     return [];
 }
 
-describe('pairSum', () => {
-    it('', () => {
-        // Arrange
-        const input = [-1, 3, 4, 2];
-        const target = 3;
-        const expected = [0, 2];
+const pairSumR = (inputs: number[], target: number) => {
+    if (inputs.length < 2) {
+        return [];
+    }
 
-        // Act
-        const actual = pairSum(input, target);
-        
-        // Assert
-        expect(actual).toEqual(expected);
+    const recurse = (index:number, inputs: number[], target:number, seen: {}) => {
+        if (index >= inputs.length) {
+            return [];
+        }
+
+        const value = inputs[index];
+        const iteration = target - value;
+        if (seen[iteration.toString()] !== undefined) {
+            return [seen[iteration], index]
+        }
+
+        return recurse(index+1, inputs, target, {...seen, [value]: index});
+    }
+
+    return inputs.length < 2 ? [] : recurse(0, inputs, target, new Map());
+}
+
+
+describe('pairSum', () => {
+    describe('pairSum', () => {
+        it('', () => {
+            // Arrange
+            const input = [-1, 3, 4, 2];
+            const target = 3;
+            const expected = [0, 2];
+    
+            // Act
+            const actual = pairSum(input, target);
+            
+            // Assert
+            expect(actual).toEqual(expected);
+        });
+    
+        it('', () => {
+            expect(pairSum([2, 7, 11, 15], 9)).toEqual([0, 1])
+        });    
+        it('', () => {
+            expect(pairSum([-1,-2,-3,-4,-5], -8)).toEqual([2,4])
+        });    
+        it('', () => {
+            expect(pairSum([-10,7,19,15], 9)).toEqual([0, 2])
+        });
     });
 
-    it('', () => {
-        expect(pairSum([2, 7, 11, 15], 9)).toEqual([0, 1])
-    });    
-    it('', () => {
-        expect(pairSum([-1,-2,-3,-4,-5], -8)).toEqual([2,4])
-    });    
-    it('', () => {
-        expect(pairSum([-10,7,19,15], 9)).toEqual([0, 2])
+    describe('pairSumR', () => {
+        it('', () => {
+            // Arrange
+            const input = [-1, 3, 4, 2];
+            const target = 3;
+            const expected = [0, 2];
+    
+            // Act
+            const actual = pairSumR(input, target);
+            
+            // Assert
+            expect(actual).toEqual(expected);
+        });
+    
+        it('', () => {
+            expect(pairSumR([2, 7, 11, 15], 9)).toEqual([0, 1])
+        });    
+        it('', () => {
+            expect(pairSumR([-1,-2,-3,-4,-5], -8)).toEqual([2,4])
+        });    
+        it('', () => {
+            expect(pairSumR([-10,7,19,15], 9)).toEqual([0, 2])
+        });
     });
 });
